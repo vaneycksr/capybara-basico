0d8f26d09639e038e176966ef152b14bf617b346
vaneycksr <vaneyck23@gmail.com>
2020-05-18 22:48:05 -0300

    mudando o foco para iframe e modal

---
+++ features/specs/iframe_e_modal.feature
@@ -0,0 +1,9 @@
+@iframe_e_modal
+Feature: Utilizando iframe e modal
+
+
+Scenario: Usando iframe
+When entro no iframe e preenche os campos
+
+Scenario: Usando modal
+When entro no modal e ferifico o texto
---
+++ features/step_definitions/iframe_e_modal.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+When('entro no iframe e preenche os campos') do
+  visit '/mudancadefoco/iframe'
+
+  # muda o foco para o iframe
+  # pode ser apenas o nome do frame na busca
+  within_frame('id_do_iframe') do
+    # acoes dentro do iframe
+
+    find('#first_name').set 'van'
+    find('#last_name').set 'rosas'
+  end
+end
+
+When('entro no modal e ferifico o texto') do
+  visit '/mudancadefoco/modal'
+
+  # clica no botao do modal
+  find('a[href="#modal1"]').click
+
+  # pega elemento pelo id
+  within('#modal1') do
+    # verifica se dentro do modal existe esse texto
+    page.assert_text(text, 'Modal Teste')
+
+    # clica no botao fechar do modal
+    find('.modal-close').click
+  end
+end
